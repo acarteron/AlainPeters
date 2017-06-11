88a6048ff46ed400cc869be0a5f0de41e5064cb7
Adrien Carteron <adcarter@localhost.localdomain>
2017-06-11 23:39:08 +0200

    [ServicesKabare:AlainPeters] useless file removing

--- src/files/libsrc.mk
+++
@@ -1,7 +0,0 @@
-SRCFILE=$(wildcard $(ROOT)/$(DIR_SRC)/files/*.cpp)
-FIL_LIB=$(SRCFILE:$(ROOT)/$(DIR_SRC)/files/%.cpp=$(ROOT)/$(DIR_OBJ)/file_%.o)
-
-# Compilation from sources (.cpp) to objects (.o)
-$(ROOT)/$(DIR_OBJ)/file_%.o: $(ROOT)/$(DIR_SRC)/files/%.cpp
-	@echo "Compiling lib target ... : $@"
-	@$(GXX) $(CFLAGS_DYNAMIC_LIBS_OBJ) $(CFLAGS_OBJ) -c -o $@ $< 
